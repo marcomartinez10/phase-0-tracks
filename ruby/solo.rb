ec12fc5f242032bdbd8ad4d368d2d9ba6a4a5956
Marco Martinez <marcomartinez94@gmail.com>
2016-06-25 18:37:46 -0400

    re-took and improved solo challenge week 6

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,39 +1,87 @@
-class Soccer_shoes 
+# This class will create baseball players, which will have
+# a name, age, position and will belong to a team (all of these
+# will be determined by the user). These players will have a 
+# chance at bat, in which they can bat a hit, a homerun, or a strike, if
+# they bat a hit they will run towards a base and then they will slide,
+# after the slide they can be out or safe.
+answer = ''
+array = []
+cont = 1
 
-	attr_accessor :size, :color, :brand
-	 @instances_of_Incomes = Array.new
+class Players
 
-def initialize (size, color, brand)
-@size=size
-@color=color
-@brand=brand
-@instances_of_Incomes = Array.new
+	attr_reader :name
+	attr_accessor :team
+
+	def initialize (team, age, position, name)
+		@team = team
+		@age = age
+		@position = position
+		@name = name
+	end
+
+	def runs
+		puts "the player runs fast towards the next base!!"
+	end
+
+	def bats (outcome)
+		if (outcome == "hit")
+			puts "the player bats a hit!!"
+		elsif (outcome == "homerun")
+			puts "and its ouuut of heree!!"
+		elsif (outcome == "strike")
+			puts "the player misses the ball!"
+		end
+	end
+
+	def slides (result)
+		puts "oh the player is #{result}!!"
+	end
 end
 
-def self.addIncome
-    size = prompt "What is the size?"
-    color = prompt "What is the color?"
-    brand = prompt "What is the brand?"
-    @@instances_of_Incomes << Incomes.new(size, color, brand)
-  end
 
-def shoot
-puts "booom!"
+
+while (answer != "quit")
+
+puts "Welcome! please create you player!"
+array << "----PLAYER #{cont}----"
+cont += 1
+puts "Please add the name of your player!"
+name = gets.chomp
+array << "NAME: #{name}"
+puts "Please add the age of your player!"
+age = gets.chomp.to_i
+array << "AGE: #{age}"
+puts "Please add the position of your player!"
+position = gets.chomp
+array << "POSITION: #{position}"
+puts "Please add the team of your player!"
+team = gets.chomp
+array << "TEAM: #{team}"
+
+player = Players.new(team, age, position, name)
+
+puts "#{name} is at bat!"
+puts "will the player hit a homerun, a hit or will he strike?"
+outcome = gets.chomp
+player.bats(outcome) 
+if (outcome=="hit") 
+	player.runs
+	puts "will the player be out or safe?"
+	result = gets.chomp
+	player.slides(result)
 end
-		 module Shoots
-			def scores (number)
-			puts "you scored the #{number} goal!"
-			end
 
-			def misses (number)
-  			puts "you just missed the #{number} goal!"
-			end
 
-        end
-Soccer_shoes.instances.each do |shoes|
-  puts shoe.size
+puts "do you want to add another player? type continue or quit"
+answer = gets.chomp
 end
-include Shoots
+
+array.each {|i| puts "#{i}"} 
 
 
-end
+
+
+
+
+
