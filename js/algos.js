ab1ce01aa48fdacb50188de9d1dac572b061e45f
Marco Martinez <marcomartinez94@gmail.com>
2016-06-26 12:11:57 -0400

    finish javascript tasks

--- js/algos.js
+++ js/algos.js
@@ -1,66 +1,93 @@
 // RELEASE 0
-// Check what word is longer in the array, store that word in a variable and check with
-// the next word and so on
+// Declare a variable with no length (great), then iterate through the string array
+// comparing first with this variable, if it is greater in length (which obviously it will be)
+// store that array in the variable (great), then the loop will compare the next string with the
+// new value of the great variable, after looping through the whole array, the longest string will be
+// stored in this variable(great), return that variable!
 
 function longest(array){
 
 var great = ''
 for (i=0; i < array.length; i++)
 
-if (array[i].length > great.length){
-
-	great = array[i];
+	if (array[i].length > great.length){
+		great = array[i];
+	}
+		return great;
 }
-return great;
 
-}
+console.log("----RELEASE 0------");
+
+console.log(longest(["long phrase","longest phrase","longer phrase"]));
+console.log(longest(["long phrase long longer longest","longest phrase a little","longer phrase"]));
+
 
 function check(object1, object2) {
 	var bool = false;
+	
 
-	for (var i in object1) {
+	for (var propt in object1) {
 		
-		if (object1[i] == object2[i]) {
-			
+		if (object1[propt] == object2[propt]) {
 			bool = true;
+		
+			
 		}
+
 	}
 	return bool;
 }
 
+ 
+console.log("----RELEASE 1------");
+console.log(check({horse: "Marty", color: "blue"}, {horse: "Bobby", color: "blue"}))
+// different keys
+console.log(check({horse: "Marty", color_different: "blue"}, {horse: "Bobby", color: "blue"}))
+// different values
+console.log(check({horse: "Marty", color: "red"}, {horse: "Bobby", color: "blue"}))
+
+
+
+console.log("----RELEASE 2----")
+
+
+//create a random char 
+function random_char() {
+	var alphabet = "abcdefghijklmnopqrstuvqxyz";
+	var random_number_chars = Math.floor(Math.random()*25+1);
+	return alphabet[random_number_chars];
+}
+
+// create a string of random length (1-10) with random chars
+function random_word() {
+	var string = "";
+	var random_number_word = Math.floor(Math.random()*10+1);
+	for (var i=0; i<random_number_word; i++) {
+		string += random_char();
+	}
+	return string;
+}
+
+// create an array of random length (user input) with random strings 
+
 function newArray(number){
-
-	//create array of random number
-
-array = array[number];
-
-// create a random number between 1 and 10
-
-var random = Math.floor((Math.random() * 10) + 1);
-
-var charset = "abcdefghijklmnopqrstuvwxyz";
-
-for (j=0; j<number; j++){
-var string = '';
-
-
-for (i=0; i < random; i++){
-
-var string = string + charset.charAt((Math.random() * 20) + 1);
-
+var array=[];
+for (i=0; i<number; i++){
+array[i] = random_word();
 }
-array.push(string);
+return array;
+}
+ //print the array of random length of strings of random chars and random length
+cont=0;
+ while (cont<10){
+ var new_random_array = newArray(3)
+console.log(new_random_array);
+
+console.log("LONGEST:" +longest(new_random_array));
+cont++;
 }
 
 
-console.log(array);
 
 
-}
 
-newArray(3);
-
-
-console.log(longest(["long phrase","longest phrase","longer phrase"]));
-
-console.log(check({horse: "Marty", color: "blue"}, {horse: "Bobby", color: "blue"}));
