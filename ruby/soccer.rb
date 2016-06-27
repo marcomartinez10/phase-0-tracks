84837e0a4cf87083ced072bf19852025480a341c
Marco Martinez <marcomartinez94@gmail.com>
2016-06-26 21:30:36 -0400

    really advanced

--- ruby/soccer.rb
+++ ruby/soccer.rb
@@ -1,11 +1,10 @@
 ## 8.5 ASSIGNMENT
-
 require 'sqlite3'
-
 db = SQLite3::Database.new("soccer.db")
 
 puts "Welcome to SOCCER app, here you can join soccer matches 
 10 vs 10 in Brooklyn, Manhattan or Queens! Join before the slots are full!!"
+
 puts "Please enter your name!"
 name = gets.chomp
 puts "Please enter your field! Brooklyn (1) Manhattan (2) Queens (3) "
@@ -17,21 +16,26 @@ end
 
 create_player(db, name, field)
 
-players = db.execute ("SELECT * FROM players")
-p players
+# players = db.execute ("SELECT * FROM players")
+# p players
 
-slots_available = db.execute("SELECT * FROM slots_available")
-p slots_available
+slots_available = db.execute("SELECT * FROM slots_available WHERE id='#{field}'")
+puts "SLOTS AVAILABLE: " 
+p slots_available[1]
+
 
 
 def update_slots(db, field)
-db.execute("UPDATE slots_available SET quantity = (SELECT quantity FROM slots_available 
-WHERE id='#{field}') -1 WHERE id='#{field}'")
+	db.execute("UPDATE slots_available SET quantity = (SELECT quantity FROM slots_available 
+				WHERE id='#{field}') -1 WHERE id='#{field}'")
 end 
 
 
 update_slots(db, field)
 
-slots_available = db.execute("SELECT * FROM slots_available")
-p slots_available
+
+
+
+# slots_available = db.execute("SELECT * FROM slots_available")
+# p slots_available
 
